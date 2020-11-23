3644a9713d6d64c2e2cf89b83d9f418c0067d020
ibwalii <ibrahimwali4u@gmail.com>
2020-11-23 23:51:44 +0100

    custom error pages

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,7 +5,6 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import Config
 
-
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
@@ -25,9 +24,11 @@ def create_app(config_class=Config):
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
